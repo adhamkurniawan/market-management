cc2b327e42211652cb19eec089c83248ad83f340
Adham Kurniawan <dmkrnwn@gmail.com>
2017-10-05 11:59:34 +0700

    adding features product, category, role, flash_message, seeds data

---
+++ app/controllers/categories_controller.rb
@@ -0,0 +1,33 @@
+class CategoriesController < ApplicationController
+  before_action :authenticate_user!
+
+  def index
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+    def find_category
+    end
+
+    def category_params
+    end
+
+    def
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register' }
+
+  resources :products
+  resources :categories
+  resources :users, only: [:index, :show]
   root 'pages#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
