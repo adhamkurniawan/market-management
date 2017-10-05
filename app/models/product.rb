b6ffe4d15a0064ad0f789c38eb257cc32b8477a1
Adham Kurniawan <dmkrnwn@gmail.com>
2017-10-05 18:22:06 +0700

    added missing image and paperclip

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,8 @@
 class Product < ApplicationRecord
+  include Paperclip::Glue
   belongs_to :user
   belongs_to :category
+
+  has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.svg"
+  validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 end
