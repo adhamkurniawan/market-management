e4c43d497ddc42d8437b421f83688f921359db98
Adham Kurniawan <dmkrnwn@gmail.com>
2017-10-17 07:14:41 +0700

    test fix bugs

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -96,6 +96,8 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :mailgun
   config.action_mailer.mailgun_settings = {
 		api_key: ENV["mailgun_api_key"],
-		domain: ENV["mailgun_domain"]
+		domain: ENV["mailgun_domain"],
+    authentication: 'plain',
+    enable_starttls_auto: true
   }
 end
